8f31d1eda7a9fe2f1d0fbd3a4a99d7ffabc42a8b
Xinjie1992 <84917323@qq.com>
2018-12-07 08:31:50 +0800

    add api

--- minerva/models.py
+++ minerva/models.py
@@ -10,6 +10,17 @@ class Build(models.Model):
     image = models.CharField(max_length=100)
     app_name = models.CharField(max_length=20)
     commit = models.CharField(max_length=1000, null=True,blank=True)
+    version_id = models.IntegerField(null=True, blank=True)
 
 class App(models.Model):
     app_name = models.CharField(max_length=20)
+
+class Version(models.Model):
+    gmt_create = models.DateTimeField('保存日期', default=timezone.now)
+    gmt_modify = models.DateTimeField('最后修改日期', auto_now=True)
+    version = models.CharField(max_length=20)
+    is_delete = models.CharField(max_length=20,null=True, blank=True,default='N')
+
+class Relation(models.Model):
+    version_id = models.IntegerField(null=True, blank=True)
+    app_id = models.IntegerField(null=True, blank=True)
--- minerva_api/view.py
+++ minerva_api/view.py
@@ -3,7 +3,8 @@ import json
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.core import serializers
-from minerva.models import Build,App
+from django.db.models import Q
+from minerva.models import Build,App,Version,Relation
 
 
 @require_http_methods(["GET"])
@@ -15,7 +16,9 @@ def addApp(req):
         resp = {"hasError": "true", "message": "应用已存在!"}
     else:
         App(app_name=app_name).save()
-        resp = {"hasError": "false", "message": "success"}
+        appId = App.objects.only('id').get(app_name=app_name)
+        data = {"id":str(appId.id),"app_name":app_name}
+        resp = {"hasError": "false", "message":data }
     return HttpResponse(json.dumps(resp), content_type="application/json")
 
 @require_http_methods(["GET"])
@@ -46,6 +49,8 @@ def getBuild(req):
     app_name = req.GET.get('appName')
     if not app_name:
         list = Build.objects.all()
+        for item in list:
+            print(item.__dict__)
         data = serializers.serialize("json", list)
         return HttpResponse(data)
     else:
@@ -53,3 +58,38 @@ def getBuild(req):
         data = serializers.serialize("json", list)
         return HttpResponse(data)
 
+
+@require_http_methods(["GET"])
+def getVersion(req):
+    list = Version.objects.filter(Q(is_delete='N')|Q(is_delete='n'))
+    data = serializers.serialize("json", list)
+    return HttpResponse(data)
+
+@require_http_methods(["GET"])
+def addVersion(req):
+    version_name = req.GET.get('version')
+    if not version_name:
+        resp = {"hasError": "true", "message": "缺少版本号!"}
+    elif (Version.objects.filter(Q(version=version_name),Q(is_delete='N')|Q(is_delete='n')).count() >= 1):
+        resp = {"hasError": "true", "message": "版本号已存在!"}
+    else:
+        Version(version=version_name).save()
+        versionId = Version.objects.only('id').get(Q(version=version_name),Q(is_delete='N')|Q(is_delete='n'))
+        data = {"id": str(versionId.id), "version": version_name}
+        resp = {"hasError": "false", "message": data}
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+@require_http_methods(["POST"])
+def setVersion(req):
+    postBody = req.body
+    json_result = json.loads(postBody)
+    print(json_result)
+    versionId = json_result['versionId']
+    appId = json_result['appId']
+    Relation.objects.filter(version_id=versionId).delete()
+    for item in appId:
+        Relation(version_id=versionId,app_id=item).save()
+    list = Relation.objects.filter(version_id=versionId)
+    data = serializers.serialize("json", list)
+    resp = {"hasError": "false", "message": data}
+    return HttpResponse(json.dumps(resp), content_type="application/json")
